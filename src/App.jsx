09008dd2f0f4ab8cd0e5b73c152a694317b28882
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2026-08-22 02:42:46 +0200

    Initial RIDEWAY project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="h-screen w-full bg-neutral-100">
+      <h1 className="p-6 text-2xl font-semibold">
+        RIDEWAY
+      </h1>
+    </main>
+  )
+}
+
+export default App
